26d08cc8630bc552c74c5fb914e77afdd78d9f36
Daniel Johannesson <daniel@johannesson.cz>
2020-04-09 01:24:16 +0200

    installed axios and created component, fetch works

---
+++ src/components/ApiHandler.vue
@@ -0,0 +1,35 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+	name: 'ApiHandler',
+	mounted(){
+		this.fetchWeather()
+	},
+	data: () => ({
+		location: 'Gothenburg',
+		appKey: '9388362ecafd4f7bb4e210728200804&q=',
+		baseURL: 'https://api.weatherapi.com/v1/current.json?key=',
+		weatherCode: Number,
+		temp_C: Number,
+	}),
+	methods: {
+		fetchWeather(){
+			axios.get(this.baseURL + this.appKey + this.location)
+			.then(Response => {
+				console.log(Response);
+				console.log(Response.data);
+				this.weatherCode = Response.data.current.condition.code
+				this.temp_C = Response.data.current.temp_c
+			})
+		},
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
