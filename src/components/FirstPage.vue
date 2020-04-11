ac34a79f019944b6b1db63fefe13afb19699b4de
Daniel Johannesson <daniel@johannesson.cz>
2020-04-11 22:41:42 +0200

    Created settingsP, checkbox and input saving +emit

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -1,18 +1,22 @@
 <template>
 	<div class="content">
+		<!-- <SettingsPage v-if="showSettings"/> -->
 	<ApiHandler/>
-		<div class="weatherIcon">
-			<img src="../assets/img/icon--weather-sunny.svg" alt="weather">
+		<div class="firstPageContent">
+			<div class="weatherIcon">
+				<img src="../assets/img/icon--weather-sunny.svg" alt="weather">
+			</div>
+			<div class="greeting"> <h1> Välkommen till <br/> ActiveMe-appen, <br/> Användare!</h1> </div>
+			<p>{{ startDay }}</p>
+			<button>NU KÖR VI!</button>
 		</div>
-		<div class="greeting"> <h1> Välkommen till <br/> ActiveMe-appen, <br/> Användare!</h1> </div>
-		<p>{{ startDay }}</p>
-		<button>NU KÖR VI!</button>
 	</div>
 
 </template>
 
 <script>
 import ApiHandler from './ApiHandler'
+
 export default {
 name: 'FirstPage',
 components: {
@@ -22,7 +26,8 @@ data: () => ({
 	greeting: `Välkommen
 	till ActiveMe-app, 
 	Användare!`,
-	startDay: 'Är du redo att påbörja din arbetsdag?'
+	startDay: 'Är du redo att påbörja din arbetsdag?',
+	
 })
 }
 </script>
@@ -44,7 +49,7 @@ data: () => ({
 	.content{
 		text-align: center;		
 	}
-	.content > button{
+	.firstPageContent > button{
 		background-color: #77c9d4;
 		width: 13em;
 		height: 3em;
@@ -53,13 +58,21 @@ data: () => ({
 		box-shadow: 1px 6px #999;
 		font-weight: 600;
 		font-size: 18px;
+		cursor: pointer;
+	}
+	.firstPageContent > button:active{
+		box-shadow: 0 5px #666;
+		transform: translateY(4px);
 	}
 	h1{
 		font-weight: 600;
 	}
 	p{
-		margin-top: 5em;
+		
 		font-size: 18px;		
+	}
+	p:first-child{
+		margin-top: 5em;
 	}
 	.greeting{
 		margin-top: 15em;
