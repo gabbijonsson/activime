7e05d0d6c6aacdbcd7c092bfee88e1dbafa52485
Daniel Johannesson <daniel@johannesson.cz>
2020-04-16 02:36:13 +0200

    filter function showing all done tasks

--- src/components/FinishedTasks.vue
+++ src/components/FinishedTasks.vue
@@ -1,22 +1,41 @@
 <template>
 	<div class="content">
 		<div class="container-finishedlist"
-		v-for="task in finishedTasks" :key="task.id">
-			{{ FinishedTasks.title }}
+		v-for="task in doneTasks" :key="task.id">
+			{{ task.title }} 
 		</div>
-
+		{{errormsg}}
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'FinishedTasks',
-	methods: {
-
-	},
 	data: () => ({
-		finsishedTasks: [],
+		doneTasks: [],
+		worklist: [],
+		errormsg: '',
 	}),
+	methods: {
+		showFinished(){
+			this.doneTasks = this.worklist.filter(function(task){
+				return task.finished == true;
+			});
+			console.log('Tasks that are avaliable', this.doneTasks);			
+			
+		},
+		tasksThatAreDone(){
+			if(localStorage.getItem('worklist')){
+				this.worklist = JSON.parse(localStorage.getItem('worklist'));
+				this.showFinished()
+			}else
+			this.errormsg = 'There are no finished tasks!'
+		}
+	},
+	
+	mounted(){
+		this.tasksThatAreDone()
+	}
 }
 </script>
 
